4785ac9150b64c7747707c2e7f639740b43bd194
Edouard Eltherington <edouard.eltherington@gmail.com>
2022-09-15 17:00:14 -0700

    variable name changes

--- selectSort.py
+++ selectSort.py
@@ -3,20 +3,20 @@
     # 2. Keep track of start of secondary index. i+1 
     # 3. Iterate through the secondary array and swap current with min if need be 
 
-
-def sortNums (list):
-    n = len(list)
+def sortNums (numList):
+    n = len(numList)
     for i in range(0,n): 
-        curr_val = list[i]
+        curr_val = numList[i]
         # Find the min and swap 
         for j in range(i+1,n):
-            next_val = list[j]
+            next_val = numList[j]
             # Swap if there next value is less then current
             if (next_val < curr_val):
-                temp = list[i]
-                list[i] = list[j]
-                list[j] = temp
+                temp = numList[i]
+                numList[i] = numList[j]
+                numList[j] = temp
                 curr_val = next_val
+    return numList
 
 
 
